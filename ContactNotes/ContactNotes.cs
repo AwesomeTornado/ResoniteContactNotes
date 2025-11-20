aad05a5d8e5ec95fc4c96be08c0dae7c7803423c
AwesomeTornado <harleypeterson@gmail.com>
2025-11-20 00:36:01 -0600

    Update example words

---
+++ ContactNotes/ContactNotes.cs
@@ -0,0 +1,41 @@
+﻿using FrooxEngine.ProtoFlux;
+using HarmonyLib;
+using MonkeyLoader.Patching;
+using MonkeyLoader.Resonite;
+using MonkeyLoader.Resonite.Features.FrooxEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonkeyLoader.ContactNotes
+{
+
+    public class AssemblyInfo
+    {
+        //setup instructions (in no particular order):
+        //Find and replace all instances of "ContactNotes" with your mod name
+        //Find and replace all instances of "Choco" with your author
+        //Find and replace all instances of "ExampleDescription" with your mod description
+        //Rename the Project file (.sln), project folder, the other project file (.csproj), and your source file (.cs
+        //Replace ExampleURL with your source repo url
+        internal const string VERSION_CONSTANT = "1.0.0"; //Changing the version here updates it in all locations needed
+    }
+
+    [HarmonyPatchCategory(nameof(ContactNotes))]
+    [HarmonyPatch(typeof(ProtoFluxTool), nameof(ProtoFluxTool.OnAttach))]
+    internal class ContactNotes : ResoniteMonkey<ContactNotes>
+    {
+        // The options for these should be provided by your game's game pack.
+        protected override IEnumerable<IFeaturePatch> GetFeaturePatches()
+        {
+            yield return new FeaturePatch<ProtofluxTool>(PatchCompatibility.HookOnly);
+        }
+
+        private static void Postfix()
+        {
+            Logger.Info(() => "Postfix for ProtoFluxTool.OnAttach()!");
+        }
+    }
+}
---
+++ ContactNotes/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("ContactNotes")]
+[assembly: AssemblyProduct("ContactNotes")]
+[assembly: AssemblyDescription("A MonkeyLoader mod that adds contact notes functionality.")]
+[assembly: AssemblyCompany("__Choco__")]
+[assembly: AssemblyCopyright("Copyright © 2025 __Choco__")]
+[assembly: AssemblyVersion(MonkeyLoader.ContactNotes.AssemblyInfo.VERSION_CONSTANT)]
+[assembly: AssemblyFileVersion(MonkeyLoader.ContactNotes.AssemblyInfo.VERSION_CONSTANT)]
